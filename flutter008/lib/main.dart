02aeedc777609e497aaa2a30659a17e3e448a918
CrazyAndy <auden.sh@163.com>
2018-12-19 21:03:33 +0800

    flutter00 new

---
+++ flutter008/lib/main.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'model/post.dart';
+import 'demo/ListView_demo.dart';
+import 'demo/Drawer_demo.dart';
+import 'demo/bottom_navigation_bar_demo.dart';
+import 'demo/basic_demo.dart';
+import 'demo/layout_demo.dart';
+import 'demo/view_demo.dart';
+import 'demo/sliver_demo.dart';
+import 'demo/navigator_demo.dart';
+
+void main() => runApp(App());
+
+
+
+class App extends StatelessWidget{
+  @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // home: Home(),
+        // home:NavigatorDemo(),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Home(),
+          '/about':(context) => Page(title: 'About',)
+        },
+        theme: ThemeData(
+          primarySwatch: Colors.yellow,
+          highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+          splashColor: Colors.white70
+        ),
+      );
+    }
+}
+
+class Home extends StatelessWidget{
+
+  @override
+    Widget build(BuildContext context) {
+      
+      // TODO: implement build
+      return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            // leading: IconButton(
+            //   icon: Icon(Icons.menu),
+            //   tooltip: 'Navigation',
+            //   onPressed: ()=>debugPrint('Navigation button is pressed.'),
+            // ),
+            title: Text('CrazyAndy'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Search',
+                onPressed: ()=>debugPrint('Search button is pressed.'),
+              )
+            ],
+            elevation: 0.0,
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              indicatorColor: Colors.black54,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 1.0,
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(icon: Icon(Icons.view_quilt),)
+              ],
+            ),
+          ),
+          // body: ListViewDemo(),
+          body: TabBarView(
+            children: <Widget>[
+              ListViewDemo(),
+              BasicDemo(),
+              LayoutDemo(),
+              // ViewDemo(),
+              SliverDemo()
+            ],
+          ),
+          drawer: DrawerDemo(),
+          bottomNavigationBar: BottomNavigationBarDemo() ,
+        ),
+      );
+    }
+}
+
+
+
+class Hello extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'eee',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 50.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
