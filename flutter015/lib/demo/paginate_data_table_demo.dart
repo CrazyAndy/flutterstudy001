980443d101ca2c91795b56232046ef8a548d418a
CrazyAndy <auden.sh@163.com>
2018-12-26 20:48:30 +0800

    flutte015 new

---
+++ flutter015/lib/demo/paginate_data_table_demo.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+
+class PostDataSource extends DataTableSource {
+  final List<Post> _posts = posts;
+  int _selectedCount = 0;
+
+  @override
+  // TODO: implement rowCount
+  int get rowCount => _posts.length;
+
+  @override
+  // TODO: implement isRowCountApproximate
+  bool get isRowCountApproximate => false;
+
+  @override
+  // TODO: implement selectedRowCount
+  int get selectedRowCount => _selectedCount;
+
+  @override
+  DataRow getRow(int index) {
+    final Post post = _posts[index];
+
+    // TODO: implement getRow
+    return DataRow.byIndex(index: index, cells: <DataCell>[
+      DataCell(Text(post.title)),
+      DataCell(Text(post.author)),
+      DataCell(Image.network(post.imageUrl)),
+    ]);
+  }
+
+  void _sort(getField(post),bool ascending){
+    _posts.sort((a,b){
+      if(!ascending){
+        final c = a;
+        a = b;
+        b = c;
+      }
+      final aValue = getField(a);
+      final bValue = getField(b);
+      return Comparable.compare(aValue, bValue);
+    });
+    notifyListeners();
+  }
+}
+
+class PaginateDataTableDemo extends StatefulWidget {
+  @override
+  _PaginateDataTableDemoState createState() => _PaginateDataTableDemoState();
+}
+
+class _PaginateDataTableDemoState extends State<PaginateDataTableDemo> {
+  int _sortColumnIndex = 0;
+  bool _sortAscending = true;
+
+  final PostDataSource _postDataSource = PostDataSource();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DataTableDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: <Widget>[
+            PaginatedDataTable(
+              sortColumnIndex: _sortColumnIndex,
+              sortAscending: _sortAscending,
+              header: Text('Posts'),
+              rowsPerPage: 5 ,
+              source: _postDataSource,
+              columns: [
+                DataColumn(
+                    label: Text('Title'),
+                    onSort: (int colunmIndex, bool ascending) {
+                      _postDataSource._sort((post)=>
+                      post.title.length,ascending);
+                      setState(() {
+                        _sortColumnIndex = colunmIndex;
+                        _sortAscending = ascending;
+                      
+                      });
+                    }),
+                DataColumn(label: Text('Author')),
+                DataColumn(label: Text('Image')),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter015/lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'model/post.dart';
+import 'demo/ListView_demo.dart';
+import 'demo/Drawer_demo.dart';
+import 'demo/bottom_navigation_bar_demo.dart';
+import 'demo/basic_demo.dart';
+import 'demo/layout_demo.dart';
+import 'demo/view_demo.dart';
+import 'demo/sliver_demo.dart';
+import 'demo/navigator_demo.dart';
+import 'demo/form_demo.dart';
+import 'demo/material_components.dart';
+import 'demo/state/state_management_demo.dart';
+import 'demo/state/scope_model_demo.dart';
+import 'demo/stream/stream_demo.dart';
+
+void main() => runApp(App());
+
+
+
+class App extends StatelessWidget{
+  @override
+    Widget build(BuildContext context) {
+      // TODO: implement build
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        // home: Home(),
+        // home:NavigatorDemo(),
+        initialRoute: '/stream',
+        routes: {
+          '/': (context) => Home(),
+          '/about':(context) => Page(title: 'About',),
+          '/form':(context)=> FormDemo(),
+          '/mdc':(context) => MaterialConponents(),
+          '/state-management':(context) => StateManagementDemo(),
+          '/scope':(context) => ScopeModelDemo(),
+          '/stream':(context) => StreamDemo(),
+        },
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          highlightColor: Color.fromRGBO(255, 255, 255, 0.5),
+          splashColor: Colors.white70
+        ),
+      );
+    }
+}
+
+class Home extends StatelessWidget{
+
+  @override
+    Widget build(BuildContext context) {
+      
+      // TODO: implement build
+      return DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          backgroundColor: Colors.grey[100],
+          appBar: AppBar(
+            // leading: IconButton(
+            //   icon: Icon(Icons.menu),
+            //   tooltip: 'Navigation',
+            //   onPressed: ()=>debugPrint('Navigation button is pressed.'),
+            // ),
+            title: Text('CrazyAndy'),
+            actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.search),
+                tooltip: 'Search',
+                onPressed: ()=>debugPrint('Search button is pressed.'),
+              )
+            ],
+            elevation: 0.0,
+            bottom: TabBar(
+              unselectedLabelColor: Colors.black38,
+              indicatorColor: Colors.black54,
+              indicatorSize: TabBarIndicatorSize.label,
+              indicatorWeight: 1.0,
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.local_florist)),
+                Tab(icon: Icon(Icons.change_history)),
+                Tab(icon: Icon(Icons.directions_bike)),
+                Tab(icon: Icon(Icons.view_quilt),)
+              ],
+            ),
+          ),
+          // body: ListViewDemo(),
+          body: TabBarView(
+            children: <Widget>[
+              ListViewDemo(),
+              BasicDemo(),
+              LayoutDemo(),
+              // ViewDemo(),
+              SliverDemo()
+            ],
+          ),
+          drawer: DrawerDemo(),
+          bottomNavigationBar: BottomNavigationBarDemo() ,
+        ),
+      );
+    }
+}
+
+
+
+class Hello extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'eee',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          fontSize: 50.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
